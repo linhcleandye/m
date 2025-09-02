35558b73308400a80f746d748b48595a4b54ebe4
l63339 <l63339@outlook.com>
2025-09-02 19:35:15 +0700

    Copy Product (ex Migration and appsetting) from Complete SS8 and update

--- src/Services/Product.API/Extensions/HostExtensions.cs
+++ src/Services/Product.API/Extensions/HostExtensions.cs
@@ -1,4 +1,6 @@
+using Common.Logging;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace Product.API.Extensions;
 
@@ -12,8 +14,8 @@ public static class HostExtensions
             config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables();
-        });
-    } 
+        }).UseSerilog(Serilogger.Configure);
+    }
     
     public static IHost MigrateDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder)
     where TContext : DbContext
